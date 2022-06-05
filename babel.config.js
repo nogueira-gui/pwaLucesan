8c02fe4d9631a7715e9fe232495413c00d0e924c
GUILHERME N SILVA <gui.r13@hotmail.com>
2022-06-05 18:40:03 -0300

    babelchanges

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,14 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
-};
+const createExpoWebpackConfigAsync = require('@expo/webpack-config')
+const { getExpoBabelLoader } = require('@expo/webpack-config/utils')
+
+module.exports = async function (env, argv) {
+  // do any other webpack config edits here
+  const config = await createExpoWebpackConfigAsync(env, argv)
+
+  // unlike webpack config, which is editted then returned, apparently
+  // babel config is expected to be mutated in place
+  const loader = getExpoBabelLoader(config)
+  loader.use.options.configFile = './babel-webpack.config.js'
+
+  return config
+}
