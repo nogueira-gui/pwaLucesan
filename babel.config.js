1cde6e32f166ce24ebfc32ed521de68c9dab564f
GUILHERME N SILVA <gui.r13@hotmail.com>
2022-06-05 18:49:50 -0300

    test2

--- babel.config.js
+++ babel.config.js
@@ -1,14 +1,9 @@
-const createExpoWebpackConfigAsync = require('@expo/webpack-config')
-const { getExpoBabelLoader } = require('@expo/webpack-config/utils')
-
-module.exports = async function (env, argv) {
-  // do any other webpack config edits here
-  const config = await createExpoWebpackConfigAsync(env, argv)
-
-  // unlike webpack config, which is editted then returned, apparently
-  // babel config is expected to be mutated in place
-  const loader = getExpoBabelLoader(config)
-  loader.use.options.configFile = './babel-webpack.config.js'
-
-  return config
-}
+module.exports = function(api) {
+  // api.cache(true);
+  const platform = api.caller((caller) => caller?.platform)
+  return {
+    presets: platform === 'web' && [require.resolve('babel-plugin-react-native-web')],
+  // return {
+  //   presets: ['babel-preset-expo'],
+  };
+};
